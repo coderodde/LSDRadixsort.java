07d100d3ecaa1a32869f14b1bb79615fedac5f92
Rodion Efremov <coderodd3@gmail.com>
2024-03-11 06:55:34 +0200

    ...

--- src/main/java/com/github/coderodde/util/LSDRadixsort.java
+++ src/main/java/com/github/coderodde/util/LSDRadixsort.java
@@ -5,42 +5,65 @@ import java.util.Arrays;
 public final class LSDRadixsort {
     
     public static void sort(int[] array) {
-        int[] buffer = new int[array.length];
-        int[] counterMap = new int[256];
-        sortImpl(array, buffer, counterMap);
+        sort(array, 0, array.length);
     }
     
-    private static void sortImpl(int[] array, int[] buffer, int[] counterMap) {
-        for (int byteIndex = 3; byteIndex != 0; byteIndex--) {
+    public static void sort(int[] array, int fromIndex, int toIndex) {
+        checkRangeIndices(array.length,
+                          fromIndex,
+                          toIndex);
+        
+        int[] buffer = new int[array.length];
+        int[] counterMap = new int[256];
+        
+        sortImpl(array,
+                 buffer, 
+                 counterMap,
+                 fromIndex,
+                 toIndex);
+    }
+    
+    private static void sortImpl(int[] array,
+                                 int[] buffer, 
+                                 int[] counterMap,
+                                 int fromIndex,
+                                 int toIndex) {
+        for (int byteIndex = 0; byteIndex != 3; byteIndex++) {
             countingSortImpl(array, 
                              buffer, 
                              counterMap, 
-                             byteIndex);
+                             byteIndex,
+                             fromIndex,
+                             toIndex);
         }
         
         countingSortImplSigned(array, 
                                buffer, 
-                               counterMap);
+                               counterMap,
+                               fromIndex,
+                               toIndex);
     }
     
     private static void countingSortImpl(int[] array,
                                          int[] buffer,
                                          int[] counterMap,
-                                         int byteIndex) {
+                                         int byteIndex,
+                                         int fromIndex,
+                                         int toIndex) {
         Arrays.fill(counterMap, 0);
         
         // Count the elements:
-        for (int i = 0, sz = array.length; i != sz; i++) {
+        for (int i = fromIndex; i != toIndex; i++) {
             counterMap[extractCounterIndex(array[i], byteIndex)]++;
         }
 
         // Make the counter map accummulative:
-        for (int i = 1, sz = array.length; i != sz; i++) {
+        for (int i = 1; i != 256; i++) {
             counterMap[i] += counterMap[i - 1];
         }
         
         // Build the output array:
-        for (int i = array.length - 1; i != -1; i--) {
+        for (int i = toIndex - 1; i >= fromIndex; i--) {
             int index = extractCounterIndex(array[i], byteIndex);
             buffer[counterMap[index]-- - 1] = array[i];
         }
@@ -55,21 +78,23 @@ public final class LSDRadixsort {
     
     private static void countingSortImplSigned(int[] array,
                                                int[] buffer,
-                                               int[] counterMap) {
+                                               int[] counterMap,
+                                               int fromIndex,
+                                               int toIndex) {
         Arrays.fill(counterMap, 0);
         
         // Count the elements:
-        for (int i = 0, sz = array.length; i != sz; i++) {
+        for (int i = fromIndex; i != toIndex; i++) {
             counterMap[extractCounterIndexSigned(array[i])]++;
         }
 
         // Make the counter map accummulative:
-        for (int i = 1, sz = array.length; i != sz; i++) {
+        for (int i = 1; i != 256; i++) {
             counterMap[i] += counterMap[i - 1];
         }
         
         // Build the output array:
-        for (int i = array.length - 1; i != -1; i--) {
+        for (int i = toIndex - 1; i >= fromIndex; i--) {
             int index = extractCounterIndexSigned(array[i]);
             buffer[counterMap[index]-- - 1] = array[i];
         }
@@ -83,13 +108,39 @@ public final class LSDRadixsort {
     }
     
     private static int extractCounterIndex(int datum, int byteIndex) {
-        int ret = (datum >>> (byteIndex * 8)) & 0xff;
-        return ret;
+        return (datum >>> (byteIndex * 8)) & 0xff;
     }
     
     private static int extractCounterIndexSigned(int datum) {
         datum >>>= 24;
-        int ret = datum > 127 ? (datum & 0b0111_1111) : (datum | 0b1000_0000);
-        return ret;
+        return datum > 127 ? (datum & 0b0111_1111) : 
+                             (datum | 0b1000_0000);
+    }
+    
+    private static void checkRangeIndices(int arrayLength, 
+                                          int fromIndex,
+                                          int toIndex) {
+        if (fromIndex < 0) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "fromIndex(%d) is negative. Must be at least 0.", 
+                            fromIndex));
+        }
+        
+        if (toIndex > arrayLength) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "toIndex(%d) is too large. Must be at most %d.",
+                            toIndex, 
+                            arrayLength));
+        }
+        
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "toIndex(%d) > fromIndex(%d).", 
+                            toIndex,
+                            fromIndex));
+        }
     }
 }
--- src/main/java/com/github/coderodde/util/demo/LSDRadixsortDemo.java
+++ src/main/java/com/github/coderodde/util/demo/LSDRadixsortDemo.java
@@ -2,22 +2,52 @@ package com.github.coderodde.util.demo;
 
 import com.github.coderodde.util.LSDRadixsort;
 import java.util.Arrays;
+import java.util.Random;
 
 public class LSDRadixsortDemo {
     
+    private static final int LENGTH = 50_000_000;
+    private static final int PREFIX_SUFFIX_EXCLUSION_RANGE_LENGTH = 50;
+    
     public static void main(String[] args) {
-        int[] array1 = { -1, -2 };
+        Random random = new Random();
+        
+        long startTime = System.currentTimeMillis();
+        int[] array1 = createRandomIntegerArray(LENGTH, random);
         int[] array2 = array1.clone();
+        long endTime = System.currentTimeMillis();
         
+        System.out.printf("Built demo arrays in %d milliseconds.\n",
+                          endTime - startTime);
+        
+        startTime = System.currentTimeMillis();
         LSDRadixsort.sort(array1);
+        endTime = System.currentTimeMillis();
+        long durationRadixsort = endTime - startTime;
+        
+        System.out.printf("LSDRaidxsort took %d milliseconds.\n", 
+                          durationRadixsort);
+        
+        startTime = System.currentTimeMillis();
         Arrays.sort(array2);
+        endTime = System.currentTimeMillis();
+        long durationArraysSort = endTime - startTime;
         
-        System.out.printf("LSDRaidxsort returned: %s.\n", 
-                          Arrays.toString(array1));
+        System.out.printf("Arrays.sort took  %d milliseconds.\n", 
+                          durationArraysSort);
         
-        System.out.printf("Arrays.sort returned: %s.\n", 
-                          Arrays.toString(array2));
+        System.out.printf("Arrays agree: %b.\n",
+                          Arrays.equals(array1,
+                                        array2));
+    }
+    
+    private static int[] createRandomIntegerArray(int length, Random random) {
+        int[] array = new int[length];
         
-        System.out.printf("Arrays agree: %b.\n", Arrays.equals(array1, array2));
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt();
+        }
+        
+        return array;
     }
 }
